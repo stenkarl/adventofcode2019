f6b25655901d2d1147a549fb6d2f3e57f8674642
stenkarl <sten_karl@yahoo.com>
2019-12-06 10:24:19 -0600

    alternate way to solve day 1

--- src/day1/Day1.scala
+++ src/day1/Day1.scala
@@ -8,12 +8,30 @@ object Day1 {
     println("Day1")
     val massList = File.fromFile("src/day1/day1.txt").map(_.toLong)
 
-    val sum = massList.foldLeft(0L){(acc, mass) => acc + calculateTotal(mass)}
+    //val sum = massList.foldLeft(0L){(acc, mass) => acc + calculateTotal(mass)}
+
+    val part1 = massList.fold(0L) { (acc, mass) => acc + (mass / 3 - 2)}
+
+    val map = massList.map (recursiveCalculate)
+    val reduce = map.reduce { (first, second) => first + second}
+    val sum = map.sum
 
     println(sum)
+    println(recursiveCalculate(100756L))
   }
 
-  def calculate(mass:Long):Long = mass / 3 - 2
+  def calculate(mass:Long):Long = {
+    val m = Math.max(mass / 3 - 2, 0)
+    //println("mass " + mass + ", fuel " + m)
+    m
+  }
+
+  def recursiveCalculate(mass:Long):Long = {
+    val curFuel = calculate(mass)
+    //println("mass " + mass + ", curFuel " + curFuel)
+    if (curFuel <= 0) return 0
+    else recursiveCalculate(curFuel) + curFuel
+  }
 
   def calculateTotal(mass:Long):Long = {
     var curFuel = calculate(mass)
