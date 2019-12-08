ff3dd46ccb725ac73a7f8fda7e1b64e1b72c75e6
stenkarl <sten_karl@yahoo.com>
2019-12-08 15:05:34 -0600

    Day 6 part 2

--- src/day6/Day6.scala
+++ src/day6/Day6.scala
@@ -8,17 +8,64 @@ object Day6 {
 
   def main(args: Array[String]): Unit = {
 
-    val nodes = File.fromFile("src/day6/day6.txt").
-                    map(_.split("\\)").toList).map(n => Node(n(1), n(0)))
+    val nodes = Node("COM", null) :: File.fromFile("src/day6/day6.txt").
+      map(_.split("\\)").toList).map(n => Node(n(1), n(0)))
 
     for (n <- nodes) {
       val parentOption = nodes.find(_.value == n.parentName)
       if (parentOption.isDefined) {
         n.parent = parentOption.get
+        n.parent.children += n
       } else {
         println("Missing parent for " + n)
       }
     }
+    val com = nodes.find(_.value == "COM").get
+    println("COM " + com.printPath())
+
+    val ccb = nodes.find(_.value == "CCB").get
+    println("CCB " + ccb.printPath())
+
+    val ww8 = nodes.find(_.value == "WW8").get
+    println("WW8 " + ww8.printPath())
+
+    println("path " + com.pathTo(ww8))
+    partTwo(nodes)
+
+  }
+
+  def partTwo(nodes:List[Node]): Unit = {
+    val you = nodes.find(_.value == "YOU").get
+    val san = nodes.find(_.value == "SAN").get
+    val ccb = nodes.find(_.value == "CCB").get
+
+    println("path back " + you.printPathBackTo(ccb))
+    println("path back " + san.printPathBackTo(ccb))
+
+  }
+
+  def partTwoA(nodes:List[Node]) {
+    val you = nodes.find(_.value == "YOU").get
+    val san = nodes.find(_.value == "SAN").get
+    println(you.printPath())
+    println(san.printPath())
+
+    var found = false
+    var p = you.parent
+    while (!found) {
+      if (p.find( "SAN") != null) {
+        found = true
+        println("Found SAN at node: " + p.value)
+      } else {
+        if (p.parent == null) {
+          println("ERROR, Null parent in " + p)
+        }
+        p = p.parent
+      }
+    }
+  }
+
+  def partOne(nodes:List[Node]) = {
     val sum = nodes.foldLeft(0)((acc, n) => {
       val p = n.path()
       println("n(" + n.value + ") = " + p)
@@ -26,7 +73,6 @@ object Day6 {
     })
 
     println("sum " + sum)
-
   }
 
 }
@@ -46,6 +92,14 @@ case class Node(value:String, parentName:String, children:MutableList[Node] = Mu
     null
   }
 
+  def pathTo(node:Node):Int = {
+    if (node.value == value) return 1
+    for (ch <- children) {
+      return 1 + ch.pathTo(node)
+    }
+    return 1
+  }
+
   def path():Int = {
     var hops = 1
     var p = parent
@@ -55,4 +109,29 @@ case class Node(value:String, parentName:String, children:MutableList[Node] = Mu
     }
     hops
   }
+
+  def printPath(): String = {
+    doPrintPath(1)
+  }
+
+  def printPathBackTo(node:Node): String = {
+    doPrintPathBackTo(node, 1)
+  }
+
+  private def doPrintPathBackTo(node:Node, level:Int): String = {
+    if (parent == null || parent == node) {
+      return parent.value
+    }
+    parent.doPrintPathBackTo(node, level + 1) + " -> (" + level + ") " + value
+  }
+
+  private def doPrintPath(level:Int): String = {
+    if (parent != null) {
+      return parent.doPrintPath(level + 1) + " -> (" + level + ") " + value
+    }
+    value
+  }
+
+  @Override override def toString: String = "Node(" + value + ", children:" +
+                                          children.size + ")"
 }
